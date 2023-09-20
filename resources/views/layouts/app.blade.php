d06d07d15809b29d851b3f4bd237ef0850898c0e
bassam <beso467@gmail.com>
2023-09-20 17:17:27 +0300

    New favicon!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="icon" href="{{ asset('tuneh.png') }}" type="image/x-icon">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
